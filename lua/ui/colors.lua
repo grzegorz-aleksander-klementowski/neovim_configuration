097ed988e3cc943fb5de3a68fb83f1cf6e94240b
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-04-02 17:02:39 +0200

    DZIAŁA!!! KURDE DZIAŁA PODKREŚLENIA ORAZ TŁO TEKSTU NA KOLOROWO I SĄ DOBRE KOLORY!!! NARESZCIE!

--- lua/ui/colors.lua
+++ lua/ui/colors.lua
@@ -1,8 +1,8 @@
 vim.opt.listchars = { tab = "→ ", trail = "·", nbsp = "␣" }
-vim.opt.showcmd = true -- Show command in bottom bar
-vim.opt.laststatus = 3 -- Always show statusline (modern UI)
+vim.opt.showcmd = true                                                 -- Show command in bottom bar
+vim.opt.laststatus = 3                                                 -- Always show statusline (modern UI)
 
--- vim.opt.termguicolors = true -- Ensure true color
+vim.opt.termguicolors = true                                           -- Ensure true color
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "#ffffff" }) -- Keep transparent but set white text
 vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff", bg = "none" }) -- Make border white
 
@@ -13,15 +13,6 @@ vim.cmd([[
       highlight Folded guibg=NONE ctermbg=NONE
       highlight EndOfBuffer guibg=NONE ctermbg=NONE
       highlight SignColumn guibg=NONE ctermbg=NONE
-]])
-
-vim.cmd([[
-  highlight Normal guibg=NONE ctermbg=NONE
-  highlight NonText guibg=NONE ctermbg=NONE
-  highlight LineNr guibg=NONE ctermbg=NONE
-  highlight Folded guibg=NONE ctermbg=NONE
-  highlight EndOfBuffer guibg=NONE ctermbg=NONE
-  highlight SignColumn guibg=NONE ctermbg=NONE
 ]])
 
 vim.api.nvim_create_autocmd("ColorScheme", {
@@ -38,8 +29,4 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "#ffffff" }) -- Keep transparent but set white text
-vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff", bg = "none" }) -- Make border white
-
--- vim.cmd("colorscheme default")
--- vim.cmd("colorscheme blue")
+-- vim.cmd("colorscheme default") -- can be changed to ei. „blue”
